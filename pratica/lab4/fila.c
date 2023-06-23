f33cbf919316b34305662bf6f5e10f32f888c414
none <lucaslopeslll412@gmail.com>
2023-06-23 09:43:59 -0300

    Refatorando o lab4

--- pratica/lab4/fila.c
+++ pratica/lab4/fila.c
@@ -7,49 +7,33 @@ Fila* criarNo(void *info) {
         printf("Erro na alocação!\n");
         exit(1);
     }
-    novoNo->info = info;
+
     novoNo->prox = novoNo;
     novoNo->ant = novoNo;
+    novoNo->info = info;
+
     return novoNo;
 }
 
 void inserirFila(Fila *F, void *info) {
     Fila* novoNo = criarNo(info);
-    if(F->prox == F) {
-        F->prox = novoNo;
-        novoNo->prox = F;
-    }
-}
 
-Fila* retirarDaFila(Fila *F) {
-    Fila *aux = NULL;
-    if(F->prox != F) {
-        aux = F->prox;
-        aux->prox->ant = F;
-        F->prox = aux->prox;
-    }
-    return aux;
+    novoNo->ant = F->ant;
+    novoNo->prox = F;
+    F->ant = novoNo;
+    if(F->prox == F)
+        F->prox = novoNo;
 }
 
 void imprimirFila(Fila *F, void (*cb) (void*)) {
-    Fila* aux = F->prox;
-    if(aux == F)
-        printf("Fila vazia!\n");
-    else {
-        while(aux != F) {
-            cb(aux->info);
-            aux = aux->prox;
-        }
+    Fila *aux = F->prox;
+    while (aux != F) {
+        cb(aux->info);
+        aux = aux->prox;
     }
+    printf("\n");
 }
 
-void destruirFila(Fila *F) {
-    Fila *aux = F->ant;
-    do{
-        F->ant = aux->ant;
-        free(aux);
-        aux = F->ant;
-    }while(aux != F);
-    F->prox = F;
-}
+
+
 
--- pratica/lab4/main.c
+++ pratica/lab4/main.c
@@ -1,18 +1,17 @@
 #include "fila.h"
 
 void imprimirInt(void *info) {
-    int* info_int = (int *)info;
-    printf("%d ", *info_int);
+    printf("%d ", *((int*)info));
 }
 
 int main() {
-    Fila *F = criarNo((void*)0);
 
+    Fila *F = criarNo((void*)0);
+   
     inserirFila(F, (void*)1);
-    // inserirFila(F, (void*)2);
-    // inserirFila(F, (void*)3);
+
     
     imprimirFila(F, imprimirInt);
 
-    return 1;
+    return 0;
 }
