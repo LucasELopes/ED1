4d055292830411013cce9b623448874c60aa2d10
LucasELopes <lucaslopeslll412@gmail.com>
2023-06-25 20:58:44 -0300

    funções da questão 1 implementadas.

--- pratica/lab4/fila.c
+++ pratica/lab4/fila.c
@@ -17,23 +17,49 @@ Fila* criarNo(void *info) {
 
 void inserirFila(Fila *F, void *info) {
     Fila* novoNo = criarNo(info);
-
-    novoNo->ant = F->ant;
     novoNo->prox = F;
+    novoNo->ant = F->ant;
+    F->ant->prox = novoNo;
     F->ant = novoNo;
-    if(F->prox == F)
-        F->prox = novoNo;
+}
+
+Fila* retirarDaFila(Fila *F) {
+    Fila* aux = F->prox;
+
+    if(F->prox != F) {
+        aux->ant->prox = aux->prox;
+        aux->prox->ant = aux->ant;
+        aux->prox = aux;
+        aux->ant = aux;
+        return aux;
+    } 
+    return NULL;
+}
+
+void destruirFila(Fila *F) {
+    if(F->prox != F) {
+        Fila *aux = F->prox;
+        F->prox = F;
+        F->ant = F;
+        while(aux != F) {
+            if(aux->prox == F) {
+                free(aux);
+                break;
+            }
+            else {
+                aux = aux->prox;
+                free(aux->ant);
+            }
+        }
+    }
 }
 
 void imprimirFila(Fila *F, void (*cb) (void*)) {
     Fila *aux = F->prox;
     while (aux != F) {
-        cb(aux->info);
+        cb(&aux->info);
         aux = aux->prox;
     }
     printf("\n");
 }
-
-
-
 
